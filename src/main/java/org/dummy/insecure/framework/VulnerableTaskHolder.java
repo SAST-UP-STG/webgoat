9d50add00fd44d6b3b1ff91512a4cb8a4754e76b
sast-qa <123078419+sast-qa@users.noreply.github.com>
2023-04-19 15:36:39 +0200

    Update VulnerableTaskHolder.java

--- src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
+++ src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 // TODO move back to lesson
 public class VulnerableTaskHolder implements Serializable {
 
+    
   private static final long serialVersionUID = 2;
 
   private String taskName;
